74a37be7f9470c316aa9e162f0e0462c82c4d2da
lilKriT <lilKriTwebDev@gmail.com>
2026-02-27 19:55:26 +0100

    Added new files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppinsSans = Poppins({
+  variable: "--font-poppins-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700", "800"],
+  style: ["normal", "italic"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppinsSans.variable} antialiased`}>
         <main>{children}</main>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <p>Day DD</p>
+
+      <p>Month MM</p>
+
+      <p>Year YYYY</p>
+
+      <p>-- years -- months -- days</p>
+    </div>
+  );
 }
