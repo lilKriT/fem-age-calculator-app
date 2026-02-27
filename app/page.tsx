6f656855085e53dfea95e7783149f9b2bfdc2bdc
lilKriT <lilKriTwebDev@gmail.com>
2026-02-27 22:31:24 +0100

    Working.

--- app/page.tsx
+++ app/page.tsx
@@ -11,43 +11,54 @@ export default function Home() {
     form: "nope",
   });
 
+  const handleSubmit = () => {
+    console.log("Submitting form");
+  };
+
   return (
     <section className="min-h-dvh flex items-center justify-center px-8 py-32">
       <div className="bg-white w-4/5 max-w-210 p-16 rounded-3xl rounded-br-[10rem]">
         {/* Date form */}
-        <form className="flex gap-8">
-          <label className="label">
-            <span>Day</span>
-            <input type="text" placeholder="DD" />
-            <span className="error">{errors.days}</span>
-          </label>
+        <form className="flex flex-col">
+          <div className="flex gap-8">
+            <label className="label">
+              <span>Day</span>
+              <input type="text" placeholder="DD" />
+              <span className="error">{errors.days}</span>
+            </label>
 
-          <label className="label">
-            <span>Month</span>
-            <input type="text" placeholder="MM" />
-            <span className="error">{errors.months}</span>
-          </label>
+            <label className="label">
+              <span>Month</span>
+              <input type="text" placeholder="MM" />
+              <span className="error">{errors.months}</span>
+            </label>
 
-          <label className="label">
-            <span>Year</span>
-            <input type="text" placeholder="YYYY" />
-            <span className="error">{errors.year}</span>
-          </label>
-        </form>
-
-        {/* Decoration */}
-        <div className="relative flex justify-end items-center">
-          <div className="h-0.5 w-full bg-gray-200 rounded-full"></div>
-          <div className="bg-purple-500 p-6 rounded-full hover:bg-black motion-safe:duration-300 ease-in-out">
-            <Image
-              src={"/icon-arrow.svg"}
-              aria-hidden="true"
-              alt="Arrow Icon"
-              width={46}
-              height={44}
-            />
+            <label className="label">
+              <span>Year</span>
+              <input type="text" placeholder="YYYY" />
+              <span className="error">{errors.year}</span>
+            </label>
           </div>
-        </div>
+
+          {/* Decoration */}
+          <div className="relative flex justify-end items-center">
+            <div className="h-0.5 w-full bg-gray-200 rounded-full"></div>
+            <button
+              type="button"
+              aria-label="Submit form"
+              className="bg-purple-500 p-6 rounded-full hover:bg-black motion-safe:duration-300 ease-in-out cursor-pointer"
+              onClick={handleSubmit}
+            >
+              <Image
+                src={"/icon-arrow.svg"}
+                aria-hidden="true"
+                alt="Arrow Icon"
+                width={46}
+                height={44}
+              />
+            </button>
+          </div>
+        </form>
 
         {/* Results */}
         <div className="flex flex-col leading-[1.15] text-[clamp(3rem,8vw,4.5rem)] font-extrabold italic">
