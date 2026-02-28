a9781c6702a14724e7cbf558a817120bdb6f124a
lilKriT <lilKriTwebDev@gmail.com>
2026-02-28 03:52:50 +0100

    Date result works.

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,11 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, SubmitErrorHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { intervalToDuration } from "date-fns";
 
-// const schema = z.object({
-//   day: z.number({ error: "No day" }).min(1, "Too small").max(31, "Too big"),
-//   month: z.number({ error: "No month" }).min(1, "Too small").max(12, "Too big"),
-//   year: z
-//     .number({ error: "No year" })
-//     .min(1900, "Too small")
-//     .max(new Date().getFullYear(), "Too big"),
-// });
-
-// Refined version
 const schema = z
   .object({
     day: z.number({ error: "No day" }).min(1, "Too small").max(31, "Too big"),
@@ -47,26 +38,55 @@ const schema = z
 
 type FormValues = z.infer<typeof schema>;
 
+type AgeResult = {
+  years: number;
+  months: number;
+  days: number;
+};
+
 export default function Home() {
+  const [result, setResult] = useState<AgeResult | null>();
+
   const { register, handleSubmit, formState } = useForm<FormValues>({
     resolver: zodResolver(schema),
   });
 
-  // const onSubmit = (data: FormValues) => {
-  //   console.log("Submitting form");
-  //   console.log(data);
-  // };
-
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log("Submitting form");
     console.log(data);
+
+    const newResult = calculateAge(data);
+
+    setResult(newResult);
+  };
+
+  const onError: SubmitErrorHandler<FormValues> = (data) => {
+    // Hypothetical error handling could go here.
+    // console.log("Wrong date!");
+    setResult(null);
+  };
+
+  const calculateAge = ({ day, month, year }: FormValues) => {
+    const birth = new Date(year, month - 1, day);
+    const now = new Date();
+
+    const duration = intervalToDuration({ start: birth, end: now });
+
+    return {
+      years: duration.years ?? 0,
+      months: duration.months ?? 0,
+      days: duration.days ?? 0,
+    };
   };
 
   return (
     <section className="min-h-dvh flex items-center justify-center px-4 md:px-8 py-32">
       <div className="bg-white w-full max-w-210 py-16 px-8 md:px-16 rounded-3xl rounded-br-[10rem]">
         {/* Date form */}
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+        <form
+          onSubmit={handleSubmit(onSubmit, onError)}
+          className="flex flex-col"
+        >
           <div className="flex items-start gap-4 md:gap-8">
             <label className="label">
               <span>Day</span>
@@ -127,13 +147,15 @@ export default function Home() {
         {/* Results */}
         <div className="flex flex-col leading-[1.15] text-[clamp(3rem,8vw,4.5rem)] font-extrabold italic">
           <span>
-            <span className="text-purple-500">--</span> years
+            <span className="text-purple-500">{result?.years || "--"}</span>{" "}
+            years
           </span>
           <span>
-            <span className="text-purple-500">--</span> months
+            <span className="text-purple-500">{result?.months || "--"}</span>{" "}
+            months
           </span>
           <span>
-            <span className="text-purple-500">--</span> days
+            <span className="text-purple-500">{result?.days || "--"}</span> days
           </span>
         </div>
       </div>
